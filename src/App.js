2039149baaa98563c615a9fc3874075b756e8e17
himanshu <royalhmj@gmail.com>
2023-04-10 18:29:57 +0530

    dg

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import './App.css';
-import {BrowserRouter} from "react-router-dom";
+// import {BrowserRouter} from "react-router-dom";
 import Add from './MyComponents/Add'; 
 import Nav from './MyComponents/Nav';
 import List from './MyComponents/List';
 
 function App() {
   return (
-  
-    <BrowserRouter>
-        <Nav/>
-        <Add/>
-       <List/>
-    </BrowserRouter>
+    <>
+    <Nav/>
+    <Add/>
+   <List/>
+</>
      
   );
 }
