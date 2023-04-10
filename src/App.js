b991ebe015db903832c20d6bae096270710c159f
himanshu <royalhmj@gmail.com>
2023-04-10 18:55:04 +0530

    style1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import './App.css';
-// import {BrowserRouter} from "react-router-dom";
-import Add from './MyComponents/Add'; 
+import Add from './MyComponents/Add';
 import Nav from './MyComponents/Nav';
 import List from './MyComponents/List';
 
 function App() {
   return (
     <>
-    <Nav/>
-    <Add/>
-   <List/>
-</>
-     
+      <Nav />
+      <Add />
+      <List />
+    </>
+
   );
 }
 
